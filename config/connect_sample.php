20e085aefd4fdbbc04cc4e57e1e3f825e3711c0c
Chattamet <chattamet.sup@gmail.com>
2024-01-30 14:15:28 +0700

    first commit

---
+++ config/connect_sample.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+// Close the connection (optional, as PHP will automatically close it at the end of the script)
+$conn->close();
+?>
